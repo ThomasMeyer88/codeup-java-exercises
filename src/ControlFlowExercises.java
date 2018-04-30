6f24df9c45c8be567b33c2ef253a6144c88f3e09
Thomas <trexmeyer@gmail.com>
2018-04-30 16:54:55 -0500

    refactored into for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        long i = 2;
-        do{
-            System.out.println(i);
-            i = i*i;
-        }while(i <= 1000000);
+       for(int i = 100; i >= -10; i-=5){
+           System.out.println(i);
+       }
+       for(long i = 2; i <= 1000000; i*=i){
+           System.out.println(i);
+       }
     }
 }
