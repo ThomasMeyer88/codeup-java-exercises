9dcef0320caeda3e055f402ca28901701b104b98
Thomas <trexmeyer@gmail.com>
2018-04-30 11:30:56 -0500

    data types java

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+        String myString = "Good Morning Vietnam";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int x = 10;
+        int y = 2;
+        x/=y;
+        System.out.println(x);
+        y-=x;
+        System.out.println(y);
+
+    }
+}
+
